fa083e9282140948fd081761c66561968c59d245
Andy1407 <58524040+Andy1407@users.noreply.github.com>
2020-02-14 21:15:38 +0300

    Update reminder.py

--- reminder.py
+++ reminder.py
@@ -7,18 +7,18 @@ bot = tb.TeleBot(TOKEN)
 
 while True:
     with open('memory.txt') as m:
-        memory = m.read().split(";")
-        
+        memory = m.read().split(";")  
     for i in memory:
         memory1 = i.split(":")
-        now = datetime.datetime.now()
-        
-        deadline = datetime.datetime(int(memory1[2]), int(memory1[3]), int(memory1[4]), int(memory1[5])-3, int(memory1[6]), int(memory1[7]))
-        
-        if now >= deadline:
-            bot.send_message(memory1[0], memory1[1])
-            
-            with open('memory.txt', 'r+') as m:
-                memory_all = m.read()
-                memory_all.pop(memory.index(i))
-                m.write(memory_all)
+        if len memory1 < 8:
+            now = datetime.datetime.now()
+
+            deadline = datetime.datetime(int(memory1[2]), int(memory1[3]), int(memory1[4]), int(memory1[5])-3, int(memory1[6]), int(memory1[7]))
+
+            if now >= deadline:
+                bot.send_message(memory1[0], memory1[1])
+
+                with open('memory.txt', 'r+') as m:
+                    memory_all = m.read()
+                    memory_all.pop(memory.index(i))
+                    m.write(memory_all)
