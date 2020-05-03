cce029167ffa8ca25b545e55719905b2b03d056b
Andy1407 <58524040+Andy1407@users.noreply.github.com>
2020-05-03 15:33:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,43 @@
 from telebot.types import ReplyKeyboardRemove
 
+import pytz
+from timezonefinder import TimezoneFinder
+
 import telegramcalendar
 
 base_memory = {}
+timezone_list = {}
 
 
 def bot(bot):
+    global timezone_list
     global base_memory
 
     local_memory = {}
-    safe_memory = {}
     messages = []
     date = []
+    timezone = "UTC"
 
     @bot.message_handler(commands=['start'])
     def start_message(message):
         bot.send_message(message.from_user.id, "Enter '/reminder' to set a reminder.")
+
+    @bot.message_handler(commands=['timezone'])
+    def timezone(message):
+        msg = bot.send_message(message.from_user.id, 'please send your location')
+        bot.register_next_step_handler(msg, set_timezone)
+
+    def set_timezone(message):
+        global timezone_list
+        if message.content_type == "location":
+            tf = TimezoneFinder()
+            timezone = tf.timezone_at(lng=message.location.longitude, lat=message.location.latitude)
+            timezone_list[message.chat.id] = pytz.timezone(timezone)
+            bot.send_message(message.from_user.id, "Your location was set.")
+
+        else:
+            bot.send_message(message.from_user.id, "You didn't send your location")
+            set_timezone(message)
 
     @bot.message_handler(commands=['cancel_last'])
     def cancel_last_message(message):
@@ -35,23 +57,32 @@ def bot(bot):
     @bot.message_handler(commands=['reminder'])
     def reminder_message(message):
         global base_memory
+        global timezone_list
         if message.chat.id not in local_memory:
             local_memory[message.chat.id] = {"messages": messages.copy(), "date": date.copy()}
+        if message.chat.id not in timezone_list:
+            timezone_list[message.chat.id] = timezone
 
         msg = bot.send_message(message.from_user.id, 'please enter the message')
         bot.register_next_step_handler(msg, text_messages)
 
     def text_messages(message):
+        global timezone_list
         if message.chat.id not in local_memory:
             local_memory[message.chat.id] = {"messages": messages.copy(), "date": date.copy()}
+        if message.chat.id not in timezone_list:
+            timezone_list[message.chat.id] = timezone
         local_memory[message.chat.id]["messages"].append(message)
         bot.send_message(message.from_user.id, "choose date:", reply_markup=telegramcalendar.create_calendar())
 
     @bot.callback_query_handler(func=lambda call: True)
     def callback_query(call):
         global base_memory
+        global timezone_list
         if call.message.chat.id not in local_memory:
             local_memory[call.message.chat.id] = {"messages": messages.copy(), "date": date.copy()}
+        if call.message.chat.id not in timezone_list:
+            timezone_list[call.message.chat.id] = timezone
 
         selected, date2, time_sending = telegramcalendar.process_calendar_selection(bot, call)
         if selected:
