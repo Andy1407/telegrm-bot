796502f1c0335710127e4ea1eb34c25f0d411519
Andy1407 <58524040+Andy1407@users.noreply.github.com>
2020-05-03 08:54:03 +0300

    Update reminder.py

--- reminder.py
+++ reminder.py
@@ -9,13 +9,16 @@ def reminder(bot):
         for user in base_memory:
             for index in range(len(base_memory[user]["date"])):
                 now = datetime.datetime.now()
-                deadline = base_memory[user]["date"][index]
-                if now >= deadline:
-                    send(bot, user, base_memory[user]["messages"][index])
-                    # bot.send_message(user, base_memory[user]["message"][index])
-                    delete_list.append([user, index])
+                for i in range(len(base_memory[user]["date"][index])):
+                    deadline = base_memory[user]["date"][index][i]
+                    if now >= deadline:
+                        send(bot, user, base_memory[user]["messages"][index])
+                        delete_list.append([user, index, i])
         for i in delete_list:
-            base_memory[i[0]]["messages"].pop(i[1])
-            base_memory[i[0]]["date"].pop(i[1])
+
+            base_memory[i[0]]["date"][i[2]].pop(i[1])
+            if not base_memory[i[0]]["date"]:
+                base_memory[i[0]]["date"].pop(i[1])
+                base_memory[i[0]]["messages"].pop(i[1])
             if not base_memory[i[0]]["messages"]:
                 base_memory.pop(i[0])
