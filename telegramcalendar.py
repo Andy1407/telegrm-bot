b424a874f339c544fe111c3964fcd54f98a0fab8
Andy1407 <58524040+Andy1407@users.noreply.github.com>
2020-05-03 09:34:43 +0300

    Update telegramcalendar.py

--- telegramcalendar.py
+++ telegramcalendar.py
@@ -84,10 +84,10 @@ def process_calendar_selection(bot, message):
                               chat_id=message.message.chat.id,
                               message_id=message.message.message_id)
         ret_data = True,datetime.datetime(int(year), int(month), int(day)), []
-        ret_data[1].append(datetime.datetime(int(year), int(month), int(day), 8, 30))
-        ret_data[1].append(datetime.datetime(int(year), int(month), int(day), 12))
-        ret_data[1].append(datetime.datetime(int(year), int(month), int(day), 15, 30))
-        ret_data[1].append(datetime.datetime(int(year), int(month), int(day), 20))
+        ret_data[2].append(datetime.datetime(int(year), int(month), int(day), 8, 30))
+        ret_data[2].append(datetime.datetime(int(year), int(month), int(day), 12))
+        ret_data[2].append(datetime.datetime(int(year), int(month), int(day), 15, 30))
+        ret_data[2].append(datetime.datetime(int(year), int(month), int(day), 20))
     elif action == "PREV-MONTH":
         pre = curr - datetime.timedelta(days=1)
         bot.edit_message_text(text=message.message.text,
