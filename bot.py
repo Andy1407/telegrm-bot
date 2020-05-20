dea10e9edc1226a73ff20c90d594093584c2b108
Andy1407 <58524040+Andy1407@users.noreply.github.com>
2020-05-20 15:56:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,7 @@ from add.formatdate import FormatDate
 from add.type import record
 from add.type import view_name, number_of_reminder
 
-base_memory = {}
-timezone_list = {}
+
 editText = (False, None)
 editDate = (False, None)
 
